b701af84dd141dde92af3e613ee74a15ef28337b
Heysocialgeek <93894378+heysocialgeek@users.noreply.github.com>
2021-12-09 19:38:41 -0300

    Agregado Index.thml

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require ("path");
 app.listen (3003, () => console.log ("Servidor corriendo en puerto 3003"));
 app.use (express.static (path.resolve (__dirname, "./public")))
 
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve (__dirname,'views/index.html'))})
+
 app.get('/login', (req,res)=>{
     res.sendFile(path.resolve (__dirname,'views/login.html'))})
 
