fe6edef6d23656cb1314f5670962be85687a0da4
unknown <joaquincaggiano@gmail.com>
2021-12-27 23:53:32 -0300

    agregue pagina de listado de productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,5 +5,6 @@ const controller = require ("../controllers/productsController");
 
 router.get ("/producto", controller.product);
 router.get ("/cart", controller.cart)
+router.get ("/productList", controller.productList)
 
 module.exports = router;
