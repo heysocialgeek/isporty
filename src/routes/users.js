ac80a30209b1a0e2d6cd9109619757d6d8aac733
mariiasol <fmariasol@gmail.com>
2021-12-31 16:15:49 -0300

    mejoras en html del formulario de creacion

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,5 +5,6 @@ const controller = require ("../controllers/usersController")
 
 router.get ("/login", controller.login)
 router.get ("/register", controller.register)
+router.get ("/formularioDeCreacion", controller.formularioDeCreacion)
 
 module.exports = router
