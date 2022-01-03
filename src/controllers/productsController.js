ecfbe74c24fa4b596fbc80a2d233db726c121458
Heysocialgeek <93894378+heysocialgeek@users.noreply.github.com>
2022-01-03 17:13:22 -0300

    carpetas y rutas

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,27 +2,27 @@ const path = require ("path");
 
 const controller = {
     product: (req, res) => {
-        const productPath = path.resolve (__dirname, '../views/producto.ejs');
+        const productPath = path.resolve (__dirname, '../views/products/producto.ejs');
         return res.render (productPath)
     },
     cart: (req, res) => {
-        const cartPath = path.resolve (__dirname, "../views/cart.ejs");
+        const cartPath = path.resolve (__dirname, "../views/products/cart.ejs");
         return res.render (cartPath)
     },
     index: (req, res) => {
-        const indexPath = path.resolve (__dirname, "../views/index.ejs");
+        const indexPath = path.resolve (__dirname, "../views/products/index.ejs");
         return res.render (indexPath)
     },
     productList: (req, res) => {
-        const productListPath = path.resolve (__dirname, "../views/productList.ejs");
+        const productListPath = path.resolve (__dirname, "../views/products/productList.ejs");
         return res.render (productListPath)
     },
     productCreationForm: (req, res) => {
-        const productCreationForm = path.resolve (__dirname, "../views/formularioDeCreacion.ejs");
+        const productCreationForm = path.resolve (__dirname, "../views/products/formularioDeCreacion.ejs");
         return res.render (productCreationForm)
     },
     formularioedit: (req, res) => {
-        const formularioEditPath = path.resolve (__dirname, '../views/formularioedit');
+        const formularioEditPath = path.resolve (__dirname, '../views/products/formularioedit');
         return res.render (formularioEditPath)
 }
 
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,11 +2,11 @@ const path = require ("path");
 
 const controller = {
     login: (req, res) => {
-        const loginPath = path.resolve (__dirname, '../views/login');
+        const loginPath = path.resolve (__dirname, '../views/users/login');
         return res.render (loginPath)
     },
     register: (req, res) => {
-        const registerPath = path.resolve (__dirname, '../views/register');
+        const registerPath = path.resolve (__dirname, '../views/users/register');
         return res.render (registerPath)
     }
 }
