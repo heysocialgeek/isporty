0d6ba3297ffa39b278260b3a590f346821c42d25
Heysocialgeek <93894378+heysocialgeek@users.noreply.github.com>
2022-01-03 21:34:22 -0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,4 +6,4 @@ const controller = require ("../controllers/usersController")
 router.get ("/login", controller.login)
 router.get ("/register", controller.register)
 
-module.exports = router
+module.exports = router;
