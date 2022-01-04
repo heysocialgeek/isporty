6bdcfbbfc773d8d1c417015b03b89ebffcc9e97c
Heysocialgeek <93894378+heysocialgeek@users.noreply.github.com>
2022-01-03 21:25:30 -0300

    Limpieza

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.use (express.static (path.resolve (__dirname, "./public")))
 
 app.set("view engine", "ejs")
 
-//rutas main//
+
 const mainRoutes = require ("./src/routes/main")
 app.use ("/", mainRoutes)
 
-//rutas users//
+
 const userRoutes = require ("./src/routes/users")
 app.use ("/user", userRoutes)
 
-//rutas products//
+
 const productsRoutes = require ("./src/routes/products")
 app.use ("/", productsRoutes)
