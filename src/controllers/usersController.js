3b15874338cc9deb20af4a16a6901b13c70deac4
unknown <joaquincaggiano@gmail.com>
2022-02-09 16:00:25 -0300

    validación del formualrio de usuario y subida del usuario al json

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,20 +1,56 @@
-const path = require ("path");
-const fs = require ('fs')
+const path = require("path");
+const fs = require('fs')
+const {validationResult} = require("express-validator")
 
-//path al JSON user//
-const userJSONpath = path.resolve (__dirname, "../../data/users.json");
+
+// path al JSON user//
+const userJSONpath = path.resolve(__dirname, "../../data/users.json");
 
 // contenido de la Data Base//
-const userDB = JSON.parse (fs.readFileSync(userJSONpath,"utf-8"));
+const userDB = JSON.parse(fs.readFileSync(userJSONpath, "utf-8"));
 
 const controller = {
     login: (req, res) => {
-        const loginPath = path.resolve (__dirname, '../views/users/login');
-        return res.render (loginPath)
+        const loginPath = path.resolve(__dirname, '../views/users/login');
+        return res.render(loginPath)
     },
     register: (req, res) => {
-        const registerPath = path.resolve (__dirname, '../views/users/register');
-        return res.render (registerPath)
+        const registerPath = path.resolve(__dirname, '../views/users/register');
+        return res.render(registerPath)
+    },
+    processRegister: (req, res) => {
+        const resultValidation = validationResult(req);
+
+        if (resultValidation.errors.length > 0) {
+            return res.render(path.resolve(__dirname, '../views/users/register'), {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }
+        const generateID = () => {
+            const lastUser = userDB[userDB.length - 1];
+            const lastID = lastUser.id;
+            return lastID + 1;
+        } 
+        userDB.push({
+            id: generateID(),
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: req.body.password,
+            documento: req.body.documento,
+            FechaDeNacimiento: req.body.birthday_date,
+            calle: req.body.calle,
+            numeroCalle: req.body.numeroCalle,
+            pisoDepartamento: req.body.piso,
+            codigoPostal: req.body.codigoPostal,
+            infoAdicional: req.body.infoAdicional,
+            image: req.file ? req.file.filename : '',
+
+        });
+
+        fs.writeFileSync(userJSONpath, JSON.stringify(userDB, null, ' '))
+        res.redirect('/')
     }
 }
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,51 @@
-const express = require ("express");
+const express = require("express");
 const router = express.Router();
+const path = require("path")
+const multer = require("multer")
+const { body } = require("express-validator")
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve(__dirname, "../../public/img/users/") )
+    },
+    filename: (req, file, cb) => {
+        let fileName = Date.now() + "_img" + path.extname(file.originalname)
+        cb(null, fileName )
+    } 
+})
+
+const uploadFile = multer({ storage })
 
 const controller = require ("../controllers/usersController")
 
+const validations = [
+    body("first_name").notEmpty().withMessage("Tienes que escribir un nombre"),
+    body("last_name").notEmpty().withMessage("Tienes que escribir un apellido"),
+    body("email")
+         .notEmpty().withMessage("Tienes que escribir un correo electrónico").bail()
+         .isEmail().withMessage("Debes escribir un formato de correo válido"),
+    body("password").notEmpty().withMessage("Tienes que escribir una contraseña"),
+    body("documento").notEmpty().withMessage("Tienes que escribir un documento de identidad"),
+    body("avatar").custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = [".jpg", ".png", ".gif"];
+        if(!file) {
+            throw new Error("Tienes que subir una imagen")
+        } else {
+            let fileExtensions = path.extname(file.originalname)
+            if (!acceptedExtensions.includes(fileExtensions)) {
+                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(", ")}`)
+            }
+        }
+        return true;
+    })
+]
+
 router.get ("/login", controller.login)
+
 router.get ("/register", controller.register)
+
+//Procesa el registro//
+router.post("/register",uploadFile.single("avatar"), validations, controller.processRegister)
 
 module.exports = router;
