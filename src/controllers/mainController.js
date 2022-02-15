e56e8c45b83872e2982b99aa39953fcd97fb16f7
Facundo-Mayuri <m.facundo@hotmail.com>
2022-02-15 19:39:09 -0300

    pagina index ya es dinamica

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,13 @@
+const fs = require("fs")
 const path = require ("path");
+
+const productJSONpath = path.resolve(__dirname, "../../data/products.json");
+const productsDB = JSON.parse(fs.readFileSync(productJSONpath, "utf-8"));
 
 const controller = {
     main: (req, res) => {
         const mainPath = path.resolve (__dirname, '../views/index.ejs');
-        return res.render(mainPath)
+        return res.render(mainPath, { productsDB })
     }
 }
 
