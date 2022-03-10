9ba6e50e733953a540916fc305a387720e504556
unknown <joaquincaggiano@gmail.com>
2022-03-09 23:12:11 -0300

    agregue multer de products a la nueva ruta de db

--- src/routes/routesdb/productRoutedb.js
+++ src/routes/routesdb/productRoutedb.js
@@ -1,11 +1,13 @@
 const express = require ("express");
 const router = express.Router();
 const productControllerdb = require("../../controllers/controllersdb/productControllerdb")
+//multerProduct
+const upload = require("../../../middlewares/multerProductMiddleware")
 
 //Mostrar todos los productos
 router.get("/list", productControllerdb.list)
 //Creación de productos
 router.get("/create", productControllerdb.create)
-router.post("/create", productControllerdb.store)
+router.post("/create", upload.single('image'), productControllerdb.store)
 
 module.exports = router;
