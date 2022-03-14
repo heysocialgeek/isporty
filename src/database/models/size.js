8e9c05f0c2778a708fbd17ee5f9d4ba48c38f027
unknown <joaquincaggiano@gmail.com>
2022-03-14 13:36:23 -0300

    detail andando

--- src/database/models/size.js
+++ src/database/models/size.js
@@ -13,7 +13,7 @@ module.exports = function (sequelize, dataTypes) {
     }
 
     let config = {
-        // tableName:"sizes",
+        tableName:"sizes",
         timestamps: true,
         createdAt: 'createdAt',
         updatedAt: 'updatedAt'
