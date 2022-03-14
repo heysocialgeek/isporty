5d775636e6d5b809cee59a190e15187183e6701c
Facundo-Mayuri <m.facundo@hotmail.com>
2022-03-13 21:04:31 -0300

    fix multer ya se cargan imgs

--- middlewares/multerProductMiddleware.js
+++ middlewares/multerProductMiddleware.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const multerDiskStorage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, path.resolve(__dirname, '../../public/img/products/'));
+		cb(null, path.resolve(__dirname, '../public/img/products/'));
 	},
 	filename: function (req, file, cb) {
 		cb(null, Date.now() + '_img' + path.extname(file.originalname));
