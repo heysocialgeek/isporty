221708a36524770055278cee87ed77f36bdd3135
Facundo-Mayuri <m.facundo@hotmail.com>
2022-03-22 21:17:49 -0300

    pagina main renderizada se muestran products

--- src/controllers/controllersdb/mainControllerdb.js
+++ src/controllers/controllersdb/mainControllerdb.js
@@ -1,8 +1,14 @@
 const db = require ("../../database/models");
 
 const mainControllerdb = {
-    index: (req,res) =>{
-        return res.render("index")
+    index: async (req,res) => {
+        const genders = await db.Gender.findAll({});
+        const brands = await db.Brand.findAll({});
+        const sizes = await db.Size.findAll({});
+        const categories = await db.Category.findAll({});
+        const colors = await db.Color.findAll({});
+        const productsDB = await db.Product.findAll({/*include: ["brands", "categories", "sizes", "gender", "colors"]*/ });
+        return res.render("index", { productsDB, genders, brands, sizes, categories, colors })
     }
 
 }
