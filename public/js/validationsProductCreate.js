cfc40ccf21adf0073ee8db8c45e796c7aa2cbe43
mariiasol <fmariasol93@gmail.com>
2022-04-11 17:17:12 -0500

    arreglos validacion product create

--- public/js/validationsProductCreate.js
+++ public/js/validationsProductCreate.js
@@ -13,6 +13,7 @@ window.addEventListener("load", function () {
 
     const selectImage = document.querySelector("[name=image]")
 
+    //validacion nombre
     productNameField.addEventListener("blur", (e) => {  
     const field = e.target;
     const spanTagError=field.nextElementSibling;
@@ -27,7 +28,7 @@ window.addEventListener("load", function () {
           
         }
     }); 
-
+    //validacion precio 
     productPriceField.addEventListener("blur", (e) => {
     const field = e.target;
     const price = field.value;
@@ -43,23 +44,23 @@ window.addEventListener("load", function () {
         }
     });
 
-
+    //validacion descripcion
     productDescriptionField.addEventListener('blur', (e) => {
     const field = e.target;
     const spanTagError = field.nextElementSibling;
-        if (field.value.trim().length > 21) {
+        if (field.value.trim().length < 21) {
             field.style.border = 'red 2px solid';
             spanTagError.innerText = `El campo descripción debe contener al menos 20 caracteres`;
             spanTagError.style.color = "red"
         } else {
             field.style.border = 'green 2px solid';
             spanTagError.style.color = "green";
-            spanTagError.innerText = "válido (front)"
+            spanTagError.innerText = "válido"
 
         }
     });
    
-
+    //validacion genero y marca
     const validateSelectOption = (e) => {
         const field = e.target;
         const spanTagError = field.nextElementSibling
@@ -75,28 +76,12 @@ window.addEventListener("load", function () {
         }
     }
     
-    const validateMultipleSelectOption = (e) => {
-
-        const field = e.target;
-        const spanTagError = field.nextElementSibling
-        console.log(field.value)
-        if (field.value === "") {
-            spanTagError.innerText = `Debes seleccionar una opción`;
-            field.style.border = 'red 2px solid';
-            spanTagError.style.color = "red"
-        } else {
-            field.style.border = 'green 2px solid';
-            spanTagError.style.color = "green";
-            spanTagError.innerText = `Opción válida `
-
-        }
-    }
+    
     productGenderField.addEventListener("blur", validateSelectOption)
     productBrandField.addEventListener("blur", validateSelectOption)
-    productSizeField.addEventListener("blur", validateMultipleSelectOption)
-    productColorField.addEventListener("blur", validateMultipleSelectOption)
-    productCategoryField.addEventListener("blur", validateMultipleSelectOption)
 
+
+    //validacion imagen
     selectImage.addEventListener("change", (e) => {
         const field = e.target;
         const image = field.value;
@@ -114,7 +99,7 @@ window.addEventListener("load", function () {
             }
     });
 
-
+    //validacion del formulario
     productCreateForm.addEventListener("submit", function (e){
         let Error= 0;
         const formFields =[...productCreateForm.elements];
