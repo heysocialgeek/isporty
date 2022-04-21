882309125a4c89f141489cb585c1f3d1ac882aa4
Joaquin Caggiano <joaquincaggiano@gmail.com>
2022-04-21 19:01:17 -0300

    cambios en APIs

--- src/controllers/controllersAPI/productControllerAPI.js
+++ src/controllers/controllersAPI/productControllerAPI.js
@@ -1,42 +1,57 @@
 const db = require("../../database/models");
-const { Op } = require("sequelize");
 
 const controllerProduct = {
     showProducts: (req, res) => {
         db.Product.findAll()
         .then(async products => {
-            const brands = await db.Brand.findAll({include: ["products"]})
-            let productsByBrand = {}
+            const brands = await db.Brand.findAll({include: ["products"]});
+
+            let productsByBrand = {};
+            let brandById = {};
+
             for (let i = 0; i < brands.length; i++) {
-                productsByBrand[brands[i].name] = brands[i].products.length
+                productsByBrand[brands[i].name] = brands[i].products.length;
+                brandById[brands[i].id] = brands[i].dataValues.name;
             }
+
             return res.status(200).json({
                 total: products.length,
                 countByBrand: {...productsByBrand},
-                data: {
+                data: { 
                     product: products.map(product => {
                         delete product.dataValues.createdAt
                         delete product.dataValues.updatedAt
                         delete product.dataValues.deletedAt
                         delete product.dataValues.price
-                        delete product.dataValues.image
+                        product.dataValues.image = `http://localhost:3003/img/products/${product.image}`
+                        delete product.dataValues.genderId
+                        delete product.dataValues.userId
+                        product.dataValues.brands = brandById[product.dataValues.brandId]//pasar el valor a STRING() si no funciona
+                        delete product.dataValues.brandId
                         product.dataValues.detail = `http://localhost:3003/api/products/${product.id}`
                     return product}),
-                    // relaciones: []
                 }
             })
         })
     },
+    
     detailProducts: (req, res) => {
+        
         db.Product.findByPk(req.params.id, {include:["sizes", "colors", "categories", "gender"]})
-            .then( product => {
+            .then( async product => {
+                const brands = await db.Brand.findAll({include: ["products"]});
+                let brandById = {};
+                for (let i = 0; i < brands.length; i++) {
+                    brandById[brands[i].id] = brands[i].dataValues.name;
+                }
                 return res.status(200).json({
                     data: {
                         id: product.id,
                         name: product.name,
                         price: product.price,
                         description: product.description,
-                        gender: product.gender.name
+                        gender: product.gender.name,
+                        brands: brandById[product.dataValues.brandId]
                     },
                     relación: [{
                         color: product.colors.map(oneColor => {
--- src/controllers/controllersAPI/userControllerAPI.js
+++ src/controllers/controllersAPI/userControllerAPI.js
@@ -4,7 +4,6 @@ const controllerUser = {
     showUsers: (req, res) => {
         db.User.findAll({})
         .then(users => {
-            // delete users.password;
             return res.status(200).json({
                 total: users.length,
                 users:
@@ -25,9 +24,13 @@ const controllerUser = {
         db.User.findByPk(req.params.id)
         .then(user => {
             delete user.dataValues.password
+            delete user.dataValues.cartId
+            delete user.dataValues.createdAt
+            delete user.dataValues.updatedAt
+            delete user.dataValues.deletedAt
             return res.status(200).json({
                 data: user,
-                URL: `http://localhost:3003/img/users/${user.image}`
+                imageURL: `http://localhost:3003/img/users/${user.image}`
             })
         })
     }
