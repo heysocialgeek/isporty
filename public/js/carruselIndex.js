15002e3369b76e04edaeb17ec6b024a372b66a8e
Heysocialgeek <93894378+heysocialgeek@users.noreply.github.com>
2022-04-23 13:55:32 -0300

    Fix arrow previous

--- public/js/carruselIndex.js
+++ public/js/carruselIndex.js
@@ -1,6 +1,6 @@
 $('.responsive').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
@@ -28,8 +28,6 @@ $('.responsive').slick({
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+      
     ]
   });
