11581c2568bed6d7e292a90e391ec286fa6b7bec
Facundo-Mayuri <m.facundo@hotmail.com>
2022-04-25 16:06:25 -0300

    fix database en middleware

--- middlewares/autoLogMiddleware.js
+++ middlewares/autoLogMiddleware.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+/*const fs = require('fs')
 const path = require("path");
 const userJSONpath = path.resolve(__dirname, "../data/users.json");
 const userDB = JSON.parse(fs.readFileSync(userJSONpath, "utf-8"));
@@ -14,4 +14,31 @@ function autoLogMiddleware (req, res, next) {
     next();
 }
 
-module.exports = autoLogMiddleware;
+module.exports = autoLogMiddleware;*/
+
+
+const db = require("../src/database/models")
+const userDB = db.User
+
+function autoLogMiddleware(req, res, next) {
+    let emailInCookie = req.cookies.userEmail
+    console.log("holaaaaaaaaaa", emailInCookie)
+    if (emailInCookie != undefined) {
+        userDB.findOne({
+            where: {
+                email: emailInCookie
+            }
+        })
+            .then(userFromCookie => {
+                req.session.userLogged = userFromCookie
+                next();
+
+            })
+        }else {
+            next();
+        }
+
+    }
+
+
+module.exports = autoLogMiddleware
